847d5529eeda23a46d9b753b26675f46e9d63c95
Ian Emsens <ian.emsens@flowpilots.com>
2018-02-01 13:49:02 +0100

    Save: [2a, 2b]

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -2,12 +2,24 @@ import { Action } from '@ngrx/store';
 
 import { Pizza } from '../../models/pizza.model';
 
-export const MY_ACTION = '[Feature] My Action';
+export const LOAD_PIZZAS = '[Pizza] Loading Pizza';
+export const LOAD_PIZZAS_FAIL = '[Pizza] Could not load Pizza';
+export const LOAD_PIZZAS_SUCCESS = '[Pizza] Loaded Pizza successfully';
 
-export class MyAction implements Action {
-  readonly type = MY_ACTION;
+export class LoadPizza implements Action {
+  readonly type = LOAD_PIZZAS;
+  constructor() {}
+}
+
+export class LoadPizzaFail implements Action {
+  readonly type = LOAD_PIZZAS_FAIL;
   constructor(public payload: any) {}
 }
 
+export class LoadPizzaSuccess implements Action {
+  readonly type = LOAD_PIZZAS_SUCCESS;
+  constructor(public payload: Pizza[]) {}
+}
+
 // action types
-export type PizzasAction = MyAction;
+export type PizzasAction = LoadPizza | LoadPizzaFail | LoadPizzaSuccess;
