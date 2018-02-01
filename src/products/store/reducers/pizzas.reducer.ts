da32efcc300433fbbaae5650a0aed3126f09f21c
Ian Emsens <ian.emsens@flowpilots.com>
2018-02-01 13:53:12 +0100

    Save: [2c]

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -1,8 +1,46 @@
 import * as fromPizzas from '../actions/pizzas.action';
 import { Pizza } from '../../models/pizza.model';
 
-export interface PizzaState {}
+export interface PizzaState {
+  loaded: boolean;
+  loading: boolean;
+  pizzas: Pizza[];
+}
 
-const initialState: PizzaState = {};
+const initialState: PizzaState = {
+  loaded: false,
+  loading: false,
+  pizzas: [],
+};
 
-export function reducer() {}
+export function reducer(state: PizzaState = initialState, action: fromPizzas.PizzasAction): PizzaState {
+  switch (action.type) {
+    case fromPizzas.LOAD_PIZZAS: {
+      return {
+        ...state,
+        loading: true,
+        loaded: false,
+      }
+    }
+
+    case fromPizzas.LOAD_PIZZAS_FAIL: {
+      return {
+        ...state,
+        loading: true,
+        loaded: true,
+      }
+    }
+
+    case fromPizzas.LOAD_PIZZAS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+      }
+    }
+
+    default:
+      break;
+  }
+  return state;
+}
